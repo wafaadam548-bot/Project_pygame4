a4b7fc246c2067b386563a2a362071b85c2ac1c1
wafaadam548 <wafaadam548@gmail.com>
2026-05-31 20:55:30 -0400

    imaoort player 1 and 2 images

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import pygame as pg
+class Player:
+    def __init__(self,name,image):
+        self.image=image
+        self.name=name
+player_one_name=Player(input())
+player_image=Player(pg.image.load(" "))
+print("hellow",player_one_name.name)    
