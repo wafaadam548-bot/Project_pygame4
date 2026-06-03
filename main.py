4b1adf421e6fb17824bed02fbb233e0b9a4be889
wafaadam548 <wafaadam548@gmail.com>
2026-06-03 13:27:25 -0400

    upload more photos

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ class Screen:
 class Background:
     def __init__(self):
         self.board=pg.image.load("images/board.png")
-        self.rect=self.board.get_rect(midbottom=(1000,500))
+        self.board =pg.transform.scale(self.board, (1000, 500))
+        self.rect=self.board.get_rect(midbottom=(500,500))
 class Player2:
     def __init__(self):
         self.image=pg.image.load("images/player2.png")
         self.image=pg.transform.scale(self.image,(100,100))
-        self.rect=self.image.get_rect(midbottom=(100,500 ))
+        self.rect=self.image.get_rect(midbottom=(150,110 ))
 class Player:
     def __init__(self):
-        self.image=(pg.image.load("images/player1.png"))
+        self.image=(pg.image.load("images/player.png"))
         self.image=pg.transform.scale(self.image,(100,100))
-        self.rect=self.image.get_rect(midbottom=(70,500 ))
+        self.rect=self.image.get_rect(midbottom=(110,110 ))
 screen = Screen(1000, 500)
 player=Player()
 player2=Player2()
@@ -37,12 +38,12 @@ while True:
         screen.screen.blit(player2.image,player2.rect)
         key =pg.key.get_pressed()
         if key[pg.K_RIGHT]:
-            player.rect.x+=3
+            player.rect.x+=10
         if key[pg.K_LEFT]:
-            player.rect.x-=3
+            player.rect.x-=10
         if key[pg.K_d]:
-            player2.rect.x+=3
+            player2.rect.x+=10
         if key[pg.K_a]:
-            player2-=3      
+            player2-=10      
         pg.display.update()
         clock.tick(60)#Consistent Game Speed
