6fedb769da99fd9531eb4687d2f06eaa74c86ed8
wafaadam548 <wafaadam548@gmail.com>
2026-06-03 08:37:42 -0400

    edit the player 2 photo and blit the 2 player in the screen

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import pygame as pg
+pg.init()
+clock=pg.time.Clock()
+class Screen:
+    def __init__(self , width, height):
+        self.width=width
+        self.height=height
+        self.screen=pg.display.set_mode((width,height))
+class Background:
+    def __init__(self):
+        self.board=pg.image.load("image")
+class Player2:
+    def __init__(self):
+        self.image=pg.image.load("images/player2.png")
+        self.image=pg.transform.scale(self.image,(200,200))
+        self.rect=self.image.get_rect(midbottom=(100,500 ))
 class Player:
-    def __init__(self,name,image):
-        self.image=image
-        self.name=name
-player_one_name=Player(input())
-player_image=Player(pg.image.load(" "))
-print("hellow",player_one_name.name)    
+    def __init__(self):
+        self.image=(pg.image.load("images/player1.png"))
+        self.image=pg.transform.scale(self.image,(200,200))
+        self.rect=self.image.get_rect(midbottom=(70,500 ))
+screen = Screen(1000, 500)
+player=Player()
+player2=Player2()
+while True:
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            pg.quit()#tell python to shut down all the pygame moudel 
+            exit()#telling python to stop running the entire file
+        screen.screen.blit(player.image,player.rect)
+        screen.screen.blit(player2.image,player2.rect)
+        pg.display.update()
+        clock.tick(60)#Consistent Game Speed
