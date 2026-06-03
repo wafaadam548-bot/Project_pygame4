b90413b6c1c8e56cf8688dfa23e8c2a8d6bc51f7
wafaadam548 <wafaadam548@gmail.com>
2026-06-03 14:23:32 -0400

    making dice

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame as pg
+import random as random
 pg.init()
 clock=pg.time.Clock()
+dice_value = 1
+rolling = False
+roll_start_time = 0
 class Questions:
     def __init__(self):
         pass
@@ -24,26 +28,35 @@ class Player:
         self.image=(pg.image.load("images/player.png"))
         self.image=pg.transform.scale(self.image,(100,100))
         self.rect=self.image.get_rect(midbottom=(110,110 ))
+    black=(0,0,0)
+    white=(225,225,225)
+    x,y=(100,100)
 screen = Screen(1000, 500)
 player=Player()
 player2=Player2()
 board=Background()
+black=(0,0,0)
 while True:
     for event in pg.event.get():
         if event.type==pg.QUIT:
             pg.quit()#tell python to shut down all the pygame moudel 
             exit()#telling python to stop running the entire file
-        screen.screen.blit(board.board,board.rect)
-        screen.screen.blit(player.image,player.rect)
-        screen.screen.blit(player2.image,player2.rect)
-        key =pg.key.get_pressed()
-        if key[pg.K_RIGHT]:
-            player.rect.x+=10
-        if key[pg.K_LEFT]:
-            player.rect.x-=10
-        if key[pg.K_d]:
-            player2.rect.x+=10
-        if key[pg.K_a]:
-            player2-=10      
-        pg.display.update()
-        clock.tick(60)#Consistent Game Speed
+        if event.type==pg.KEYDOWN:
+            if event.key==pg.K_SPACE:
+                rolling=True
+                roll_start_time=pg.time.get_ticks()
+    screen.screen.blit(board.board,board.rect)
+    screen.screen.blit(player.image,player.rect)
+    screen.screen.blit(player2.image,player2.rect)
+    pg.draw.rect(screen.screen,black, (900,50,70,70))
+    key =pg.key.get_pressed()
+    if key[pg.K_RIGHT]:
+        player.rect.x+=10
+    if key[pg.K_LEFT]:
+        player.rect.x-=10
+    if key[pg.K_d]:
+        player2.rect.x+=10
+    if key[pg.K_a]:
+        player2.rect.x-=10      
+    pg.display.update()
+    clock.tick(60)#Consistent Game Speed
